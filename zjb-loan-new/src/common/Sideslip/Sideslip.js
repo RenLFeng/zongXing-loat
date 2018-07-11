9e9e002236eed24b4d02c4904c0f42e51985b92d
zjb2 <zjb2@zjb.com>
2018-07-11 15:03:04 +0800

    我的优惠券页面

--- zjb-loan-new/src/common/Sideslip/Sideslip.js
+++ zjb-loan-new/src/common/Sideslip/Sideslip.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './Sideslip.css';
 import { Card } from 'antd';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
-/** 
+//import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+/**
  * 组件还未完成
  * 还需要补充调用方式
 */
@@ -11,8 +11,8 @@ class Sideslip extends React.Component {
         this.props.closeModal();
     };
     render() {
-        const Child = 
-        this.props.visable ? 
+        const Child =
+        this.props.visable ?
         <div className="mask" onClick={()=>{this.closeModal()}}>
             <div className="contentBox">
             <Card  bordered={false} className="content" onClick={(e)=>{e.stopPropagation()}}>
@@ -21,15 +21,15 @@ class Sideslip extends React.Component {
             </div>
         </div>
         : null;
-        /** 
+        /**
          * transitionName 需要匹配的样式名称,他会按照需要的  transitionAppear   transitionEnter   transitionLeave 三种状态去添加后缀匹配样式
          * transitionAppear  组件初次加载  transitionName-appear
          * transitionEnter 组件渲染 transitionName-enter
-         * transitionLeave 组件销毁 transitionName-leave 
+         * transitionLeave 组件销毁 transitionName-leave
          * 注意  过渡动画时长不要超过500ms
         */
         return (
-            <ReactCSSTransitionGroup
+        {/*<ReactCSSTransitionGroup
                 transitionName='show'
                 transitionAppear={true}
                 transitionEnter={true}
@@ -38,7 +38,7 @@ class Sideslip extends React.Component {
                 transitionLeaveTimeout={500}
             >
             {Child}
-            </ReactCSSTransitionGroup>
+            </ReactCSSTransitionGroup>*/}
         )
     }
 }
