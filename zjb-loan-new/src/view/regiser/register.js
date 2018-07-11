8c30ce6e94065a5a5e3b897b402ae28fedda7e3a
fj <1208079349@qq.com>
2018-07-11 15:18:43 +0800

    还款计划

--- zjb-loan-new/src/view/regiser/register.js
+++ zjb-loan-new/src/view/regiser/register.js
@@ -238,7 +238,7 @@ export default class Register extends React.Component {
       fpwd: regPwd.trim(),
       // fpwd: regPwd,
       authcode:regAuthCode.trim(),
-      type: 0, //投资用户
+      type: 1, //投资用户
     };
     // 调用注册接口
     try {
